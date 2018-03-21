9dceb6afd4818a4d4e14856302fa835ada97518e
lenaraa <33811053+lenaraa@users.noreply.github.com>
2018-03-21 17:55:43 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,31 @@
 # CuteBot by Lenaraa
 
 import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
 import random
 import asyncio
 from tokens import token
-from discord.ext.commands import Bot
-from discord.ext import commands
 
 client = discord.Client()
 bot_prefix="!"
-client = commands.Bot(command_prefix=bot_prefix)
+bot = commands.Bot(command_prefix=bot_prefix)
 
-@client.event
+@bot.event
 async def on_ready():
     print("Bot online !")
-    print("Name : {}".format(client.user.name))
-    print("ID : {}".format(client.user.id))
+    print("Name : {}".format(bot.user.name))
+    print("ID : {}".format(bot.user.id))
 
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def ping(ctx):
-    await client.say("Pong !")
+    await bot.say("Pong !")
 
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def cute(ctx):
-    await client.say("image")
-    #await client.say(random.randrange(100))
+    pass
+    await bot.say("image")
+    #await bot.say(random.randrange(100))
 
 
-client.run("token");
+bot.run("token");
