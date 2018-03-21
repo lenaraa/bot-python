b511289c787bb5525704ed9f5e6931a8aa29a209
lenaraa <33811053+lenaraa@users.noreply.github.com>
2018-03-21 16:12:37 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+
+client = discord.Client()
+bot_prefix="!"
+client = commands.Bot(command_prefix=bot_prefix)
+
+@client.event
+async def on_ready():
+    print("Bot online !")
+    print("Name : {}".format(client.user.name))
+    print("ID : {}".format(client.user.id))
+
+@client.command(pass_context=True)
+async def ping(ctx):
+    await client.say("Pong !")
+
+
+
+
+client.run("NDI0MzMyNTg1MDQ5NTIyMTc2.DY3WIA.sj6epSfdz4nqadKqOekpAWGGbZA");
