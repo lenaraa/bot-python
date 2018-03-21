9a710eb5fa7dc8ca0c384bfdaa1dfa232cd74d12
lenaraa <33811053+lenaraa@users.noreply.github.com>
2018-03-21 20:04:50 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,9 +6,10 @@ from discord.ext import commands
 import random
 import asyncio
 from tokens import token
+import os
 
-client = discord.Client()
-bot_prefix="!"
+#client = discord.Client()
+bot_prefix=("!", "?")
 bot = Bot(command_prefix=bot_prefix)
 
 @bot.event
@@ -17,15 +18,57 @@ async def on_ready():
     print("Name : {}".format(bot.user.name))
     print("ID : {}".format(bot.user.id))
 
-@bot.command(pass_context=True)
-async def ping(ctx):
-    await bot.say("Pong !")
-
-@bot.command(pass_context=True)
-async def cute(ctx):
-    pass
-    await bot.say("image")
+@bot.command(name='cute',
+             description="Envoie une photo d'un animal mignon.",
+             brief="Fais le plein de mignonitude !")
+async def cute():
+    #await bot.say("image")
+    dir_fd = os.open('pic', os.O_RDONLY)
+    def opener(path, flags):
+        return os.open(path, flags, dir_fd=dir_fd)
+    liste=os.listdir("/pic")
+    image=random.choice(liste)
+    with open(image, 'rb', opener=opener) as f:
+        await bot.send_file(discord.Channel, f)
+    os.close(dir_fd) 
     #await bot.say(random.randrange(100))
 
 
-bot.run("token");
+@bot.command(name='ping',
+             description="Envoie ping",
+             brief="Ping pong")
+async def ping():
+    await bot.say("Pong !")
+
+
+@bot.command(name='8ball',
+             description="Répond à une question en oui ou non.",
+             brief="Laisse l'univers décider.",
+             aliases=['eight_ball', 'eightball', '8-ball'])
+async def eight_ball():
+    possible_resp = [
+        'Essaye plus tard',
+        'Essaye encore ',
+        'Pas d\'avis ',
+        'C\'est ton destin',
+        'Le sort en est jeté',
+        'Une chance sur deux ',
+        'Repose ta question ',
+        'D\'après moi oui ',
+        'C\'est certain',
+        'Oui absolument',
+        'Tu peux compter dessus',
+        ' Sans aucun doute ',
+        'Très probable',
+        'Oui',
+        'C\'est bien parti',
+        'C\'est non',
+        'Peu probable',
+        'Faut pas rêver',
+        'N\'y compte pas',
+        'Impossible'
+    ]
+    await bot.say(random.choice(possible_resp))
+
+
+bot.run(token);
